5d604582d8b44b74c7c7f4661c8e2e402b45e4f5
bvba <bfs@kakao.com>
2020-10-14 01:02:01 +0900

    basic controller
    hello back, current time 출력하는 기본 컨트롤러 추가

---
+++ src/main/java/com/_404/back/controller/BasicController.java
@@ -0,0 +1,20 @@
+package com._404.back.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/test")
+    public Object test() {
+        return new ResponseEntity<>("hello back!\nTime : " +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy MM dd EEE HH:mm:ss")), HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/_404/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com._404.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
