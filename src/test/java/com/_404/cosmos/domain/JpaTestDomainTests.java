aab90f21739555228ede0626f3758494806cdd79
bvba <bfs@kakao.com>
2021-01-14 22:37:34 +0900

    오탈자 수정
    Signed-off-by: bvba <bfs@kakao.com>

--- src/test/java/com/_404/cosmos/domain/JpaTestDomainTests.java
+++ src/test/java/com/_404/cosmos/domain/JpaTestDomainTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class TestDomainJpaTest {
+public class JpaTestDomainTests {
 
     @Autowired
     public JpaTestModelRepository jpaTestModelRepository;
