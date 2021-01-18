b6af8dd10f52bed7903001fcc54ed16f6374d857
bvba <bfs@kakao.com>
2021-01-19 01:59:26 +0900

    user 모델에서 쓰일 token creator util 추가
    Signed-off-by: bvba <bfs@kakao.com>

---
+++ src/main/java/com/_404/cosmos/utils/TokenCreator.java
@@ -0,0 +1,16 @@
+package com._404.cosmos.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+public class TokenCreator {
+
+	private TokenCreator() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	public static String  createToken() {
+		return UUID.randomUUID().toString();
+	}
+}
