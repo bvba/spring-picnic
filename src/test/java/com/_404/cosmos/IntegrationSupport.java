52732477f345f4b93246705b6ceb57ee3f3652a0
bvba <bfs@kakao.com>
2021-01-19 01:41:02 +0900

    junit 4 -> 5
    - junit5 에서는 RunWith 대신 ExtendWith 사용
    - 어노테이션 변경
    - junit 4 에서 사용하는 vintage 를 제외하는 gradle 설정은 스프링 부트 2.2.0 버전 이후에서는 설정하지 않아도 됨(default 가 vintage를 사용하지 않는 것임)
    
    Signed-off-by: bvba <bfs@kakao.com>

---
+++ src/test/java/com/_404/cosmos/IntegrationSupport.java
@@ -0,0 +1,10 @@
+package com._404.cosmos;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(classes = CosmosApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationSupport {
+	@LocalServerPort
+	Integer port = 0;
+}
