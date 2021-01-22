4b2bfdd75b60f1130220fadca0e5d483fe9b7fc0
dsyun96 <dsyun96@gmail.com>
2021-01-22 21:01:29 +0900

    refactor : AllArgsConstructor 어노테이션 추가

--- src/main/java/com/_404/cosmos/common/ErrorCode.java
+++ src/main/java/com/_404/cosmos/common/ErrorCode.java
@@ -1,19 +1,16 @@
 package com._404.cosmos.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@AllArgsConstructor
 public enum ErrorCode {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, 404_0001);
 
     private final HttpStatus httpStatus;
     private final int code;
-
-    ErrorCode(HttpStatus httpStatus, int code) {
-        this.httpStatus = httpStatus;
-        this.code = code;
-    }
 
 }
