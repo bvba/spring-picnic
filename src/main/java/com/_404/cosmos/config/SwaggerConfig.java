8329778bce4569d5dd096d4348ab6d12ac43e27d
bvba <bfs@kakao.com>
2021-01-25 22:41:10 +0900

    api prototype 정의 및 더미 테스트 코드 작성 (#14)
    Signed-off-by: bvba <bfs@kakao.com>

---
+++ src/main/java/com/_404/cosmos/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com._404.cosmos.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	SwaggerConfig() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+		ModelConverters.getInstance().addConverter(new ModelResolver(mapper));
+	}
+}
---
+++ src/main/java/com/_404/cosmos/controller/response/ReplyScore.java
@@ -0,0 +1,9 @@
+package com._404.cosmos.controller.response;
+
+import lombok.Data;
+
+@Data
+public class ReplyScore {
+	private Double lightOnScore = 0D;
+	private Double patienceScore = 0D;
+}
