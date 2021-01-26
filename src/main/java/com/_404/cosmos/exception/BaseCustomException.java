fd8d53ab5f213bf9b5b606977972ba39c66df466
dsyun96 <dsyun96@gmail.com>
2021-01-26 21:54:35 +0900

    refactor : AllArgsConstructor 적용

--- src/main/java/com/_404/cosmos/exception/BaseCustomException.java
+++ src/main/java/com/_404/cosmos/exception/BaseCustomException.java
@@ -1,20 +1,14 @@
 package com._404.cosmos.exception;
 
 import com._404.cosmos.common.ErrorCode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
-@Slf4j
+@AllArgsConstructor
 public class BaseCustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
     private final String message;
-
-    public BaseCustomException(ErrorCode errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message;
-        log.info("[Error] " + errorCode.getHttpStatus() + ": " + message);
-    }
 
 }
--- src/main/java/com/_404/cosmos/exception/GlobalExceptionHandler.java
+++ src/main/java/com/_404/cosmos/exception/GlobalExceptionHandler.java
@@ -1,17 +1,29 @@
 package com._404.cosmos.exception;
 
+import com._404.cosmos.common.ErrorCode;
 import com._404.cosmos.controller.response.ReplyError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseBody
     public ReplyError handleBadRequestException(BadRequestException e) {
+        log.info("[Error] " + e.getErrorCode() + ": " + e.getMessage());
         return new ReplyError(e);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ReplyError handleBadRequestException(MethodArgumentNotValidException e) {
+        log.info("[Error] " + e.getParameter() + ": " + e.getMessage());
+        return new ReplyError(new BaseCustomException(ErrorCode.INVALID_PARAMETER, e.getMessage()));
     }
 
 }
