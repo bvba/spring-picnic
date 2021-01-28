bbe17b27252cd689091e370d9c41974f50839da3
YunGoon <dsyun96@gmail.com>
2021-01-28 23:01:04 +0900

    refactor : 코드 리포맷 (#20)

--- src/main/java/com/_404/cosmos/utils/TokenCreator.java
+++ src/main/java/com/_404/cosmos/utils/TokenCreator.java
@@ -1,16 +1,14 @@
 package com._404.cosmos.utils;
 
-import org.springframework.stereotype.Component;
-
 import java.util.UUID;
 
 public class TokenCreator {
 
-	private TokenCreator() {
-		throw new IllegalStateException("Utility class");
-	}
+    private TokenCreator() {
+        throw new IllegalStateException("Utility class");
+    }
 
-	public static String createToken() {
-		return UUID.randomUUID().toString();
-	}
+    public static String createToken() {
+        return UUID.randomUUID().toString();
+    }
 }
