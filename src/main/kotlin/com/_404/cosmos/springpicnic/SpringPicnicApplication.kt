77b4a599d49c845e84453b115206079b9abc21a6
bvba <bfs@kakao.com>
2022-08-20 11:43:44 +0900

    feat: make dockerfile

--- src/main/kotlin/com/_404/cosmos/springpicnic/SpringPicnicApplication.kt
+++ src/main/kotlin/com/_404/cosmos/springpicnic/SpringPicnicApplication.kt
@@ -2,10 +2,21 @@ package com._404.cosmos.springpicnic
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @SpringBootApplication
-class SpringPicnicApplication
+@RestController
+class SpringPicnicApplication {
+    @GetMapping("/home")
+    fun home(): String {
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<SpringPicnicApplication>(*args)
+    runApplication<SpringPicnicApplication>(*args)
 }
