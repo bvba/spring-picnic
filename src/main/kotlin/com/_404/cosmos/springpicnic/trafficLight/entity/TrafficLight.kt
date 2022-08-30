986a83fced9918a02947eaa66a80b8fbb4fc4368
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-08-30 21:20:58 +0900

    create traffic_light.kt
    create traffic_light entity

--- src/main/kotlin/com/_404/cosmos/springpicnic/trafficLight/entity/TrafficLight.kt
+++ src/main/kotlin/com/_404/cosmos/springpicnic/trafficLight/entity/TrafficLight.kt
@@ -1,12 +1,11 @@
-package com._404.cosmos.springpicnic.passerby.entity
+package com._404.cosmos.springpicnic.trafficLight.entity
 
 import com._404.cosmos.springpicnic.user.entity.User
 import javax.persistence.*
 
-
 @Entity
-@Table(name = "passerby")
-open class Passerby (
+@Table(name = "traffic_light")
+open class TrafficLight (
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     open var id: Long? = null,
@@ -18,6 +17,10 @@ open class Passerby (
     @Column(name = "pos")
     open var pos: Int? = null,
 
-    @Column(name = "arrival_time")
-    open var arrivalTime: Int? = null
+    @Column(name = "is_green_light")
+    open var isGreenLight: Boolean = false,
+
+    @Column(name = "green_light_maintained_time")
+    open var greenLightMaintainedTime : Int = 0
+
 )
