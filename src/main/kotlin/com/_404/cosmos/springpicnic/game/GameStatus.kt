1c2ee8cd4b73ab4b018968ff9ffbf125b8e84480
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-09-01 10:23:33 +0900

    Update GameStatus.kt
    대소문자 정정

--- src/main/kotlin/com/_404/cosmos/springpicnic/game/GameStatus.kt
+++ src/main/kotlin/com/_404/cosmos/springpicnic/game/GameStatus.kt
@@ -3,5 +3,5 @@ package com._404.cosmos.springpicnic.game
 enum class GameStatus(val status: String) {
     NONE("none"),
     PROGRESS("progress"),
-    Completed("completed")
+    COMPLETED("completed")
 }
