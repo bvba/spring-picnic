2b665ed381884b2b1d383f2cf39572c617b6e19d
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-09-02 14:00:32 +0900

    유저 삭제
    플레이어로 변경되었으므로 유저 삭제

--- src/main/kotlin/com/_404/cosmos/springpicnic/user/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com._404.cosmos.springpicnic.user.repository
-
-import com._404.cosmos.springpicnic.user.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
-
-interface UserRepository : JpaRepository<User,Long?> {
-    fun findByAuthToken(authToken: String) : Optional<User>
-}
